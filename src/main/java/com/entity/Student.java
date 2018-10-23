bdd996a844c09b76818f34dcd7d6380d38b63304
lisi <943571986@qq.com>
2018-10-23 18:13:55 +0800

    修改代码

---
+++ src/main/java/com/entity/Student.java
@@ -0,0 +1,18 @@
+package com.entity;
+
+public class Student {
+	private Integer sid;
+	private String sname;
+	public Integer getSid() {
+		return sid;
+	}
+	public void setSid(Integer sid) {
+		this.sid = sid;
+	}
+	public String getSname() {
+		return sname;
+	}
+	public void setSname(String sname) {
+		this.sname = sname;
+	}
+}
